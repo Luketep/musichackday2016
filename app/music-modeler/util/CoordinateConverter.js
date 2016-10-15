668f98f0f7d27ca468d49727a71f6ab360c1635a
Askar Akhmerov <askar.akhmerov@camunda.com>
2016-10-15 23:00:49 +0200

    add crow

--- app/music-modeler/util/CoordinateConverter.js
+++ app/music-modeler/util/CoordinateConverter.js
@@ -31,6 +31,28 @@ function getAngleBetweenGeoLocations(lat1, lng1, lat2, lng2) {
     return Math.abs(lng2 - lng1) / Math.sqrt(Math.abs(lng2 - lng1)) + Math.sqrt(Math.abs(lat2 - lat1));
 }
 
+//This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
+function calcCrow(lat1, lon1, lat2, lon2)
+{
+    var R = 6371; // km
+    var dLat = toRad(lat2-lat1);
+    var dLon = toRad(lon2-lon1);
+    var lat1 = toRad(lat1);
+    var lat2 = toRad(lat2);
+
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
+    return d;
+}
+
+// Converts numeric degrees to radians
+function toRad(Value)
+{
+    return Value * Math.PI / 180;
+}
+
 function mapGeoDistance(x) {
     // y = (x-A)/(B-A) * (D-C) + C
     
@@ -73,7 +95,7 @@ function getTargetPosition(origin, lat1, lng1, lat2, lng2) {
     var distance = mapGeoDistance(geoDistance);
     var angle = getAngleBetweenGeoLocations(lat1, lng1, lat2, lng2);
     var target = getPointForDistance(distance, angle, origin);
-    
+    var dist = calcCrow(lat1,lng1,lat2,lng2);
     return target;    
 }
 
